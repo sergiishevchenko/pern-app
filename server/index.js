917ec9c501e5285ffc34ec3f59cee09966a1b11a
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-10-17 18:33:08 +0200

    finish version

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-// const models = require('./models/models')
+const models = require('./models/models')
 const cors = require('cors')
-// const fileUpload = require('express-fileupload')
-// const router = require('./routes/index')
-// const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const fileUpload = require('express-fileupload')
+const router = require('./routers/index')
+const errorHandler = require('./middlewares/ErrorHandlingMiddleware')
 const path = require('path')
 
 const PORT = process.env.PORT || 5000
@@ -15,10 +15,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
-// app.use(fileUpload({}))
-// app.use('/api', router)
+app.use(fileUpload({}))
+app.use('/api', router)
 
-// app.use(errorHandler)
+app.use(errorHandler)
 
 const start = async () => {
     try {
