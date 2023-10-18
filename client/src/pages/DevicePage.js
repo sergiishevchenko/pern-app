76a59d1b30e4a7cb659de45f70d957901b10e371
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-10-18 15:01:27 +0200

    added dependency id

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -11,7 +11,7 @@ const DevicePage = () => {
 
     useEffect(() => {
         fetchOneDevice(id).then(data => setDevice(data))
-    }, [])
+    }, [id])
 
     return (
         <Container className="mt-3">
