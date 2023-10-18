883ba7ae9aad405326cea5b41c6adb7db9b73c50
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-10-18 14:52:58 +0200

    Switch -> Routes

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import {authRoutes, publicRoutes} from "../routes";
 import {SHOP_ROUTE} from "../utils/consts";
 import {Context} from "../index";
@@ -9,17 +9,16 @@ import {observer} from "mobx-react-lite";
 const AppRouter = observer(() => {
     const {user} = useContext(Context)
 
-    console.log(user)
     return (
-        <Switch>
+        <Routes>
             {user.isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} component={Component} exact/>
             )}
             {publicRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} component={Component} exact/>
             )}
-            <Redirect to={SHOP_ROUTE}/>
-        </Switch>
+            <Route to={SHOP_ROUTE}/>
+        </Routes>
     );
 });
 
