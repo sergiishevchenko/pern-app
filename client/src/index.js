dd2f4aedb40a9872b1038bd226ba2c5aeb334125
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-10-18 14:52:37 +0200

    refactored

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+export const Context = createContext(null)
+
+ReactDOM.render(
+    <Context.Provider value={{
+        user: new UserStore(),
+        device: new DeviceStore(),
+    }}>
+        <App />
+    </Context.Provider>,
+  document.getElementById('root')
 );
